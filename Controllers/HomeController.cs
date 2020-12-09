85e3010ccc89dcb9c8745046c8a7b514bd44a0cc
Robert Yearling <laserguy@mac.com>
2020-12-09 17:58:40 -0600

    Finished Files

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,16 +11,76 @@ namespace ViewModelFun.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        [HttpGet]
+        [Route("")]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        [Route("users")]
+        public IActionResult Users()
+        {
+            Individual personOne = new Individual()
+            {
+                FirstName = "Moose",
+                LastName = "Phillips"
+            };
+            Individual personTwo = new Individual()
+            {
+                FirstName = "Sarah",
+                LastName = ""
+            };
+            Individual personThree = new Individual()
+            {
+                FirstName = "Jerry",
+                LastName = ""
+            };
+            Individual personFour = new Individual()
+            {
+                FirstName = "Renee",
+                LastName = "Ricky"
+            };
+            Individual personFive = new Individual()
+            {
+                FirstName = "Barbarah",
+                LastName = " "
+            };
+
+            List<Individual> viewModel = new List<Individual>()
+            {
+                personOne, personTwo, personThree, personFour, personFive
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpGet("numbers")]
+        public IActionResult Numbers()
+        {
+            int[] ListONums = new int[] {1, 2, 3, 10, 43, 5};
+            Number numberOne = new Number()
+            {
+                NumberList = ListONums
+            };
+            return View(numberOne);
+        }
+
+        [HttpGet("user")]
+
+        public IActionResult User()
+        {
+            Person onePerson = new Person()
+            {
+                FirstName = "Moose",
+                LastName = "Phillips"
+            };
+
+            ViewBag.User = onePerson;
+
+            return View(onePerson);
         }
 
         public IActionResult Privacy()
